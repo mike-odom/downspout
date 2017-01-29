9197991b420f13fded5487b175b39eec1bfa0872
Odie <ThatOdieGuy@gmail.com>
2017-01-29 14:13:17 -0800

    A bit of config documentation.

--- htpc/configDefaults.js
+++ htpc/configDefaults.js
@@ -1,5 +1,12 @@
 const config = {};
 
+/**
+ * This is the FTP on your seedbox to connect to.
+ *
+ * TODO: Allow multiple FTPs to connect to.
+ *
+ * @type {{host: string, user: string, password: string, syncRoot: string, port: number}}
+ */
 config.seedboxFTP = {
     host: "localhost",
     user: "[Default]",
@@ -8,14 +15,30 @@ config.seedboxFTP = {
     port: 21
 };
 
+/**
+ * Where you are syncing to on the local machine
+ *
+ * @type {string}
+ */
 config.localSyncFolder = "~/somePath";
 
+/**
+ * Download config
+ *
+ * @type {{countMax: number, speedMax: number}}
+ */
 config.downloads = {
     countMax: 2,
     speedMax: 300000
 };
 
-//You'll normally want to keep this to true, this is for testing purposes only.
+/**
+ * The seedbox script creates symlinks. This deletes those symlinks.
+ *
+ * You'll normally want to keep this to true, this is for testing purposes only.
+ *
+ * @type {boolean}
+ */
 config.deleteRemoteFiles = true;
 
 module.exports = config;
