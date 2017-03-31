8564cabf0fa12632a062a0ecc8637a2c9df9b0c8
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2017-03-31 12:32:10 -0700

    Fixing first word of torrent becoming an empty directory.

--- seedbox/testSmall.sh
+++ seedbox/testSmall.sh
@@ -8,3 +8,4 @@ rm -R toUpload/*
 # Folder with .avi and .srt
 ./deluge.sh "Small Movie" "SmallMovie.avi" ${baseMediaDir}/movies
 ./deluge.sh "Small Show" "SmallShow.avi" ${baseMediaDir}/tv
+./deluge.sh "Whatever Season" "Whatever Season 1" ${baseMediaDir}/tv
