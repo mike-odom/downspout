9cfc47c82ad95302c118eaa900af8e5dde8578c2
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2017-04-05 13:14:49 -0700

    Converting console.* messages to logger.*

--- htpc/src/ts/app.ts
+++ htpc/src/ts/app.ts
@@ -116,7 +116,7 @@ app.use(function (err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    console.log(err);
+    logger.log(err);
 
     // render the error page
     res.status(err.status || 500);
