266cca0efbd8cc1496df165f82969cfecd6f9384
Odie <ThatOdieGuy@gmail.com>
2017-04-06 21:17:32 -0700

    Cleanup

--- htpc/src/client/app.js
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { DownloadItemList } from "./Components/DownloadItemList";
-
-ReactDOM.render(React.createElement(DownloadItemList, null), document.getElementById('app-container'));
