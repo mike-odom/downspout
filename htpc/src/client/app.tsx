8adb89ab1dedbb85161140545f81b4b11d8a99eb
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2017-04-06 21:20:25 -0700

    Moving files from Components -> components

--- htpc/src/client/app.tsx
+++ htpc/src/client/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {DownloadItemList} from './Components/DownloadItemList';
+import {DownloadItemList} from './components/DownloadItemList';
 
 //import { Router, IndexRoute, Route, browserHistory } from 'react-router';
 
